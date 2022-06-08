0ede893fc318af933baa903077d780e3db221a34
SebastianSeczkowski <37137@studentwsei.pl>
2022-06-09 00:00:16 +0200

    Realizacja punktu 5

---
+++ MyDB.java
@@ -0,0 +1,60 @@
+package pl.lublin.wsei.java.cwiczenia.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+import java.util.Properties;
+
+public class MyDB{
+
+    private String user;
+    private String password;
+    private String serverName;
+    private String portNumber;
+    private String database;
+    private Connection conn = null;
+
+    public MyDB(String serverName, String database, Number portNumber);
+        this.serverName = serverName;
+        this.database = database;
+        this.portNumber = portNumber;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private void connect() {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", user);
+        connectionProps.put("password", password);
+        connectionProps.put("serverTimezone","Europe/Warsaw");
+
+        String dbName;
+        String host;
+        String port;
+        String jdbcString = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+        try {
+            conn = DriverManager.getConnection(
+                    jdbcString, connectionProps);
+        }
+        catch (SQLException e) {
+            System.out.println("Błąd podłączenia do bazy: ")+jdbcString);
+            System.out.println("Komunikat błędu: "+e.getMessage());
+            conn = null;
+        }
+        System.out.println("Connected to database "+dbName);
+    }
+    
+    public Connection getConnection() {
+    if (conn == null)
+        connect();
+    
+    return conn;
+
+}
+
